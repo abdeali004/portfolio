03a72a55418fb8806032c59aea5a492e17761352
Abdeali Stationwala <abdeali52.station@gmail.com>
2024-06-02 20:29:41 +0530

    Redifined main and certificates page to a newer look. Also added the latest certificates and shifted certificates folder to images.

---
+++ js/certificates.js
@@ -0,0 +1,113 @@
+const modal = document.getElementById("myModal");
+const modalImg = document.getElementById("modalImage");
+const captionText = document.getElementById("modalCaption");
+const certificatesDictionary = getCertificatesDictionary();
+let index1 = 0;
+let index2 = 0;
+let rowString = "";
+
+// Windows Onload
+window.onload = function () {
+  blockViewSource();
+  parseCertificatesOnload();
+};
+
+// Parse Certificate Images  
+function parseCertificatesOnload() {
+  var finalResult = "";
+  var index1 = 0;
+  // Loop through the certificatesDictionary
+  for (const category in certificatesDictionary) {
+    rowString = "";
+    if (certificatesDictionary.hasOwnProperty(category)) {
+      index1 += 1;
+      var certificates = certificatesDictionary[category];
+      // console.log(`Category: ${category}`);
+      certificates.forEach((certificate, index2) => {
+        if (certificate != "") {
+          // console.log(`- ${certificate}`);
+          rowString += `
+                        <div class="col-2 card-row">
+                          <div class="card h-100" data-bs-theme="dark" style="width: 10rem;">
+                            <div class="card-body">
+                              <p class="card-text" id="certificateTitleSection${index1}${index2}">${certificate}</p>
+                            </div>
+                            <img src="./images/certificates/${certificate}.jpg" class="card-img-top" alt="${certificate}.jpg" id="certificateImageSection${index1}${index2}"
+                              onclick="imageViewModal(this)">
+                          </div>
+                        </div>
+                      `;
+        }
+      });
+      finalResult += `<div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button bg-default collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapse${index1}" aria-expanded="true" aria-controls="collapse${index1}">
+                ${category}
+              </button>
+            </h2>
+            <div id="collapse${index1}" class="accordion-collapse collapse" data-bs-parent="#certificateAccordionView">
+            <div class="accordion-body">
+            <div class="row">
+            ${rowString}
+            </div>
+            </div>
+            </div>
+            </div>`;
+    }
+  }
+  document.getElementById("certificateAccordionView").innerHTML = finalResult;
+}
+
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+function imageViewModal(img) {
+  modal.style.display = "block";
+  modalImg.src = img.src;
+  captionText.innerHTML = "Data Analysis With Python";
+}
+
+// Get the <span> element that closes the modal
+// When the user clicks on <span> (x), close the modal
+document.getElementsByClassName("close")[0].onclick = closeImageModal();
+
+// Close modal function
+function closeImageModal() {
+  modal.style.display = "none";
+}
+
+// Reading Keyboard formations + Extras
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+function blockViewSource() {
+  document.addEventListener(
+    "contextmenu",
+    (e) => {
+      e.preventDefault();
+    },
+    false
+  );
+
+  document.addEventListener("keydown", (e) => {
+    // USE THIS TO DISABLE CONTROL AND ALL FUNCTION KEYS
+    // if (e.ctrlKey || (e.keyCode>=112 && e.keyCode<=123)) {
+    // THIS WILL ONLY DISABLE CONTROL AND F12
+    if (e.key === "Escape") {
+      closeImageModal();
+    }
+    if (
+      e.keyCode === 123 ||
+      ctrlShiftKey(e, "I") ||
+      ctrlShiftKey(e, "J") ||
+      ctrlShiftKey(e, "C") ||
+      (e.ctrlKey && e.keyCode === "U".charCodeAt(0)) ||
+      (e.ctrlKey && e.keyCode >= 112)
+    ) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+  });
+}
---
+++ js/certificatesContent.js
@@ -0,0 +1,65 @@
+
+function getCertificatesDictionary() {
+
+    var certificatesDictionary = {
+        "Java": [
+            "Java Basics (Hackerrank)",
+            "Build Automation with Maven (LinkedIn)",
+            "Essentials Syntax and Structure (LinkedIn)",
+            "Essentials Objects and APIs (LinkedIn)",
+            "Spring Boot for Beginners"
+        ],
+        "Python": [
+            "Python Basics (Hackerrank)",
+            "Crash Course on Python (Coursera)",
+            "Building Web in Django (Coursera)",
+            "MTA Python (Microsoft)",
+            "Python for Data Science (IBM)",
+            "Web Application and Technologies in Django (Coursera)"
+        ],
+        "Databases": [
+            "PostgreSQL Essential Training (LinkedIn)"
+        ],
+        "Tools and Frameworks": [
+            "Git and Github (Coursera)"
+        ],
+        "Cloud": [
+            "Introduction to Cloud (IBM)",
+            "Introduction to Cloud Identity Google (Coursera)",
+            "AWS Machine Learning (Coursera)",
+            "Cloud Computing Basics (Coursera)",
+            "Conversational Experience with Dialogflow",
+            "Azure Fundamentals-AZ-900 (Microsoft)"
+        ],
+        "Mathematics": [
+            "Introduction to Calculus (Coursera)",
+            "Data Science Maths Skills (Coursera)",
+            "Statistics 101 (IBM)"
+        ],
+        "Data Science": [
+            "What is Data Science (Coursera)",
+            "Introduction to Data Science (IBM)",
+            "Data Analysis with Python (IBM)",
+            "Data Science Methodologies (IBM)",
+            "Data Science Tools (IBM)",
+            "Data Visualization with Python (IBM)",
+            "Data Visualization with R (IBM)",
+            "Machine Learning with Python (IBM)"
+
+        ],
+        "Google": [
+            "Digital Marketing",
+            "Python to Interact with the Operating System",
+            "Troubleshooting and Debbuging Techniques"
+        ],
+        "Others": [
+            "Scrum Foundation Professional (Certiprof)",
+            "Cyber Security Foundation Profession (Certiprof)",
+            "Introduction to Matlab (Coursera)",
+            "Marketing Analytics (Coursera)",
+            "Programming in R (IBM)"
+        ],
+    }
+
+    return certificatesDictionary;
+}
